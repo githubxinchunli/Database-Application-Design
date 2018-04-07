a28c3cb57ab8678e62d7ffce137ea2c3f7272de4
Xinchun Li <xincli@umich.edu>
2018-04-07 00:22:47 -0400

    Delete pdo.php

--- pdo.php
+++
@@ -1,5 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=8889;dbname=misc',
-    'xinc', 'li');
-// See the "errors" folder for details...
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
